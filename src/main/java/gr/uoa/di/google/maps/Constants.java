5299ea8b30865a9c7ba75f5454070ffd2233f3a7
Despina <papdespina@gmail.com>
2016-09-03 16:21:25 +0300

    new classes for mapping functionality

---
+++ src/main/java/gr/uoa/di/google/maps/Constants.java
@@ -0,0 +1,26 @@
+package gr.uoa.di.google.maps;
+
+/**
+ * Created by Despina on 3/9/2016.
+ */
+public final class Constants {
+    public static final int SUCCESS_RESULT = 0;
+    public static final int FAILURE_RESULT = 1;
+    public static final String PACKAGE_NAME =
+            "gr.uoa.di";
+    public static final String RECEIVER = PACKAGE_NAME + ".RECEIVER";
+    public static final String RESULT_DATA_KEY = PACKAGE_NAME +
+            ".RESULT_DATA_KEY";
+//        public static final String LOCATION_DATA_EXTRA = PACKAGE_NAME +
+//                ".LOCATION_DATA_EXTRA";
+//        public static final String  SUCCESS_RESULT = PACKAGE_NAME +".SUCCESS_RESULT";
+
+//    public static final int USE_ADDRESS_LOCATION = 2;
+    public static final int USE_ADDRESS_NAME = 1;
+
+    public static final String FETCH_TYPE_EXTRA = PACKAGE_NAME + ".FETCH_TYPE_EXTRA";
+
+    public static final String LOCATION_NAME_DATA_EXTRA= PACKAGE_NAME +".LOCATION_NAME_DATA_EXTRA";
+
+    public  static final String RESULT_ADDRESS= PACKAGE_NAME+".RESULT_ADDRESS";
+}
--- src/main/java/gr/uoa/di/google/maps/MapsActivity.java
+++ src/main/java/gr/uoa/di/google/maps/MapsActivity.java
@@ -2,17 +2,31 @@ package gr.uoa.di.google.maps;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.net.Uri;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import android.os.ResultReceiver;
+
+import android.util.Log;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
+import android.location.Address;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -26,12 +40,20 @@ import gr.uoa.di.R;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, GoogleMap.OnMyLocationButtonClickListener, GoogleApiClient.ConnectionCallbacks {
 
-    //
-
-
     private GoogleMap mMap;
     private GoogleApiClient mGoogleApiClient;//used for find Last current location
     private LatLng mylocation = null;
+
+    private static final String TAG = "MAIN_ACTIVITY";
+
+    int fetchType = Constants.USE_ADDRESS_NAME;
+    TextView selleraddress;
+    AddressResultReceiver mResultReceiver;
+
+
+    ProgressBar progressBar;
+    TextView infoText;
+    TextView infoText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,12 +65,21 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapFragment.getMapAsync(this);
 
         if (mGoogleApiClient == null) {
+
             mGoogleApiClient = new GoogleApiClient.Builder(this)
                     .addConnectionCallbacks(this)
-                   // .addOnConnectionFailedListener(this)
+                    // .addOnConnectionFailedListener(this)
                     .addApi(LocationServices.API)
-                    .build();
+                    .addApi(AppIndex.API).build();
         }
+
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        infoText = (TextView) findViewById(R.id.infoText);
+        infoText2 = (TextView) findViewById(R.id.infoText2);
+//       TODO:: (correct) selleraddress=(TextView) findViewById(R.id.selleraddress);
+
+        mResultReceiver = new AddressResultReceiver(null);
     }
 
 
@@ -64,6 +95,29 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         LatLng shop = new LatLng(37, 23);
         mMap.addMarker(new MarkerOptions().position(shop).title("Marker in somewhere rnadom"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(shop));
+
+
+
+        //find the loction by anddress--------------------------- instead the dummy loc
+        Intent intent = new Intent(this, FetchAddressIntentService.class);
+        intent.putExtra(Constants.RECEIVER, mResultReceiver);
+        intent.putExtra(Constants.FETCH_TYPE_EXTRA, fetchType);
+        if(fetchType == Constants.USE_ADDRESS_NAME) {
+            //selleraddress----> TODO:: 8a thn pernw apo list pou periexei tous sellers
+
+//            selleraddress.setText("Ούλωφ Πάλμε 44, T.K. 15771, Ζωγράφου");
+//            if(selleraddress.getText().length() == 0) {
+//                Toast.makeText(this, "Please enter an address name", Toast.LENGTH_LONG).show();
+//                return;
+//            }
+            intent.putExtra(Constants.LOCATION_NAME_DATA_EXTRA,"Ούλωφ Πάλμε 44, T.K. 15771, Ζωγράφου" );//TODO:: correct selleraddress.getText().toString()
+        }
+
+        infoText2.setVisibility(View.INVISIBLE);
+        progressBar.setVisibility(View.VISIBLE);
+        Log.e(TAG, "Starting Service");
+        startService(intent);
+        //------------------------------
 
 
     }
@@ -97,67 +151,46 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
-
-//    @Override
-//    public void onConnected(Bundle connectionHint) {
-//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            // TODO: Consider calling
-//            //    ActivityCompat#requestPermissions
-//            // here to request the missing permissions, and then overriding
-//            // to handle the case where the user grants the permission. See the documentation
-//            // for ActivityCompat#requestPermissions for more details.
-//            Context context = getApplicationContext();
-//            CharSequence text = "onConnected !";
-//            int duration = Toast.LENGTH_SHORT;
-//
-//            Toast toast = Toast.makeText(context, text, duration);
-//            toast.show();
-//
-//        } else {
-//            Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-//
-//            if (mLastLocation != null) {
-//                Double latitude = mLastLocation.getLatitude();
-//                Double longitude = mLastLocation.getLongitude();
-//
-//                mylocation = new LatLng(latitude, longitude);
-//
-//                Toast toast = Toast.makeText(getApplicationContext(), "i found your location" + mylocation, Toast.LENGTH_SHORT);
-//                toast.show();
-//            }
-//
-//        }
-//    }
-
-//    @Override
-//    public void onConnectionSuspended(int i) {
-//        Toast toast = Toast.makeText(getApplicationContext(), "unfortunately the connection has lost, please enable your wifi or data !! ", Toast.LENGTH_SHORT);
-//    }
-
-//    @Override
-//    public boolean onMarkerClick(Marker marker) {
-//        //when the user will click on Market it will shows some info about
-//        //TODO:: add code later
-//        Toast toast = Toast.makeText(getApplicationContext(), "A 5stars shop!! ", Toast.LENGTH_SHORT);
-//        return false;
-//    }
-
     //methods used by mGoogleApiClient
     protected void onStart() {
         mGoogleApiClient.connect();
         super.onStart();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+//        Action viewAction = Action.newAction(
+//                Action.TYPE_VIEW, // TODO: choose an action type.
+//                "Maps Page", // TODO: Define a title for the content shown.
+//                // TODO: If you have web page content that matches this app activity's content,
+//                // make sure this auto-generated web page URL is correct.
+//                // Otherwise, set the URL to null.
+//                Uri.parse("http://host/path"),
+//                // TODO: Make sure this auto-generated app URL is correct.
+//                Uri.parse("android-app://gr.uoa.di.google.maps/http/host/path")
+//        );
+//        AppIndex.AppIndexApi.start(mGoogleApiClient, viewAction);
     }
 
     protected void onStop() {
         mGoogleApiClient.disconnect();
         super.onStop();
+//do I need the next lines???
+/*        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Maps Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://gr.uoa.di.google.maps/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(mGoogleApiClient, viewAction);*/
     }
 
     @Override
     public boolean onMarkerClick(Marker marker) {
-
-        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
-        return true;
+//        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        return false;
     }
 
     @Override
@@ -185,4 +218,43 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onConnectionSuspended(int i) {
 
     }
-}//classS
+
+
+ class AddressResultReceiver extends ResultReceiver {
+
+        public AddressResultReceiver(Handler handler) {
+            super(handler);
+        }
+        @Override
+        protected void onReceiveResult(int resultCode, final Bundle resultData) {
+            if (resultCode == Constants.SUCCESS_RESULT) {
+                final Address address = resultData.getParcelable(Constants.RESULT_ADDRESS);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressBar.setVisibility(View.INVISIBLE);
+                        infoText2.setText("Latitude: " + address.getLatitude() + "\n" +
+                                "Longitude: " + address.getLongitude() + "\n" +
+                                "Address: " + resultData.getString(Constants.RESULT_DATA_KEY));
+                        double lng=address.getLongitude();
+                        double lat=address.getLatitude();
+                        LatLng shop = new LatLng(lng, lat);
+                        mMap.addMarker(new MarkerOptions().position(shop).title("Marker in seller's place"));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(shop));
+                    }
+                });
+            } else {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressBar.setVisibility(View.GONE);
+                        infoText.setVisibility(View.VISIBLE);
+                        infoText.setText(resultData.getString(Constants.RESULT_DATA_KEY));
+                    }
+                });
+            }
+        }
+
+ }//inner class
+
+}
